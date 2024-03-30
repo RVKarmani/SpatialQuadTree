2da4a081f0cd99a708eee919445a9868641ea535
flyskywalkerlby <1694584152@qq.com>
2024-03-29 21:05:59 -0400

    draw results

---
+++ draw_results.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+
+def draw_ht2num(hitting_rate, random_rate):
+    """
+    hitting rate to the number of data   
+
+    Parameters:
+    - hitting_rate: list
+    - random_rate: list
+    """
+
+
+def draw_t2r(t_naive, t_bulk, random_rate):
+    """
+    time to the random rate of data
+
+    Parameters:
+    - t_naive: list
+    - t_bulk: list
+    - random_rate: list
+    """
+
+
+def draw_tree(points):
+    pass
+
+
+    
