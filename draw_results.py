4155e8f0bdce226c8e37ff8791a90b723af180d8
flyskywalkerlby <1694584152@qq.com>
2024-03-29 21:13:34 -0400

    draw results

--- draw_results.py
+++ draw_results.py
@@ -1,7 +1,16 @@
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
 
 
-def draw_ht2num(hitting_rate, random_rate):
+def get_parser():
+    parser = ArgumentParser()
+    parser.add_argument("--task", type=str, default="m", choices=["m", "h", "t"], help="tree map, hitting rate, time")
+    
+    return parser
+
+
+
+def draw_ht2num():
     """
     hitting rate to the number of data   
 
@@ -9,9 +18,13 @@ def draw_ht2num(hitting_rate, random_rate):
     - hitting_rate: list
     - random_rate: list
     """
+    print("Drawing hitting to random rate...")
+    
+    hitting_rate = []
+    random_rate = []
 
 
-def draw_t2r(t_naive, t_bulk, random_rate):
+def draw_t2r():
     """
     time to the random rate of data
 
@@ -20,10 +33,29 @@ def draw_t2r(t_naive, t_bulk, random_rate):
     - t_bulk: list
     - random_rate: list
     """
+    print("Drawing time to random rate...")
+    
+    t_naive = []
+    t_bulk = []
+    random_rate = []
 
 
-def draw_tree(points):
-    pass
+def draw_tree():
+    print("Drawing the tree map...")
+
+    points = []
 
 
     
+def main():
+    args = get_parser().parse_args()
+    if args.task == "m":
+        draw_tree()
+    elif args.task == "h":
+        draw_ht2num()
+    elif args.task == "t":
+        draw_t2r()
+
+
+if __name__ == "__main__":
+    main()
