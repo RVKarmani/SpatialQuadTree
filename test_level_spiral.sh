e15d396ae88e6afcc1fbb7bb44b7db64592dfcdf
bch1755 <bch1755@outlook.com>
2024-04-29 16:02:52 -0400

    add hit-level spiral

---
+++ test_level_spiral.sh
@@ -0,0 +1,5 @@
+.\\quadTree.exe ./data/empty.txt -1 ./data/insert_s=0.75_curve=fermat_spiral.txt 1 0
+.\\quadTree.exe ./data/empty.txt -1 ./data/insert_s=0.75_curve=fermat_spiral.txt 1 2
+.\\quadTree.exe ./data/empty.txt -1 ./data/insert_s=0.75_curve=fermat_spiral.txt 1 4
+.\\quadTree.exe ./data/empty.txt -1 ./data/insert_s=0.75_curve=fermat_spiral.txt 1 6
+.\\quadTree.exe ./data/empty.txt -1 ./data/insert_s=0.75_curve=fermat_spiral.txt 1 8
