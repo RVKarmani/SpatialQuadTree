11753a27452ff05b6180c132b39675844d7409bd
rohitvemparala <vemparalarohit1998@gmail.com>
2024-05-01 15:16:12 -0400

    Add changes

--- cases.sh
+++ cases.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-# python3 data_query_generator.py -s=0.0 -c=bezier
-# python3 data_query_generator.py -s=0.25 -c=bezier
-# python3 data_query_generator.py -s=0.5 -c=bezier
-# python3 data_query_generator.py -s=0.75 -c=bezier
-# python3 data_query_generator.py -s=1.0 -c=bezier
+python3 data_query_generator.py -s 0.0 -c bezier -n 1000000 -d gaussian
+python3 data_query_generator.py -s 0.25 -c bezier -n 1000000 -d gaussian
+python3 data_query_generator.py -s 0.5 -c bezier -n 1000000 -d gaussian
+python3 data_query_generator.py -s 0.75 -c bezier -n 1000000 -d gaussian
+python3 data_query_generator.py -s 1.0 -c bezier -n 1000000 -d gaussian
 
-# python3 data_query_generator.py -s=0.0 -c=fermat_spiral
-# python3 data_query_generator.py -s=0.25 -c=fermat_spiral
-# python3 data_query_generator.py -s=0.5 -c=fermat_spiral
-# python3 data_query_generator.py -s=0.75 -c=fermat_spiral
-# python3 data_query_generator.py -s=1.0 -c=fermat_spiral
+python3 data_query_generator.py -s 0.75 -c fermat_spiral -n 100000 -d gaussian
+python3 data_query_generator.py -s 0.75 -c fermat_spiral -n 1000000 -d gaussian
+python3 data_query_generator.py -s 0.75 -c fermat_spiral -n 10000000 -d gaussian
+python3 data_query_generator.py -s 0.75 -c fermat_spiral -n 100000000 -d gaussian
 
-python3 data_query_generator.py -s=0.0 -c=random_curve
-python3 data_query_generator.py -s=0.25 -c=random_curve
-python3 data_query_generator.py -s=0.5 -c=random_curve
-python3 data_query_generator.py -s=0.75 -c=random_curve
-python3 data_query_generator.py -s=1.0 -c=random_curve
-
-
-# python3 data_query_generator.py -s=0.0 -c=circ_arc
-# python3 data_query_generator.py -s=0.25 -c=circ_arc
-# python3 data_query_generator.py -s=0.5 -c=circ_arc
-# python3 data_query_generator.py -s=0.75 -c=circ_arc
-# python3 data_query_generator.py -s=1.0 -c=circ_arc
+python3 data_query_generator.py -s 0.75 -c fermat_spiral -n 100000 -d uniform
+python3 data_query_generator.py -s 0.75 -c fermat_spiral -n 1000000 -d uniform
+python3 data_query_generator.py -s 0.75 -c fermat_spiral -n 10000000 -d uniform
+python3 data_query_generator.py -s 0.75 -c fermat_spiral -n 100000000 -d uniform
--- data_query_generator.py
+++ data_query_generator.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import random, argparse
-from queue import Queue
-from tqdm import tqdm
+import quads
+
+tree = quads.QuadTree(
+    (0, 0),  # The center point
+    4000,  # The width
+    2000,  # The height
+)
 
 # Curves
 BEZIER = "bezier"
@@ -13,20 +18,34 @@ CURVE_CHOICES = [BEZIER, FMT_SPR, RND_CRV, CIRC_ARC]
 
 # Distributions
 UNIFORM_DIST = "uniform"
-CLUSTERED_DIST = "clustered"
-RANDOM_DIST = "random"
-DIST_CHOICES = [UNIFORM_DIST, CLUSTERED_DIST, RANDOM_DIST]
+GAUSSIAN_DIST = "gaussian"
+DIST_CHOICES = [UNIFORM_DIST, GAUSSIAN_DIST]
 
 # Define the parser
-parser = argparse.ArgumentParser(description='Quadtree data/query generator')
-parser.add_argument('-xl', help="x low coordinate for boundary", type=int, default=-1800)
-parser.add_argument('-yl', help="y low coordinate for boundary", type=int, default=-900)
-parser.add_argument('-xh', help="x high coordinate for boundary", type=int, default=1800)
-parser.add_argument('-yh', help="y high coordinate for boundary", type=int, default=900)
-parser.add_argument('-n', help="Number of queries", type=int, default=1000000)
-parser.add_argument('-s', help="Sortedness for inserts", type=float, default=0.5)
-parser.add_argument('-c', help="Curve type for data", type=str, choices=CURVE_CHOICES, default=RND_CRV)
-# parser.add_argument('-d', help="Distribution type for data", type=str, choices=DIST_CHOICES, default=CLUSTERED_DIST)
+parser = argparse.ArgumentParser(description="Quadtree data/query generator")
+parser.add_argument(
+    "-xl", help="x low coordinate for boundary", type=int, default=-1800
+)
+parser.add_argument("-yl", help="y low coordinate for boundary", type=int, default=-900)
+parser.add_argument(
+    "-xh", help="x high coordinate for boundary", type=int, default=1800
+)
+parser.add_argument("-yh", help="y high coordinate for boundary", type=int, default=900)
+parser.add_argument("-n", help="Number of queries", type=int, default=10000)
+parser.add_argument("-s", help="Sortedness for inserts", type=float, default=0.5)
+parser.add_argument(
+    "-c", help="Curve type for data", type=str, choices=CURVE_CHOICES, default=FMT_SPR
+)
+parser.add_argument(
+    "-d",
+    help="Distribution type for data",
+    type=str,
+    choices=DIST_CHOICES,
+    default=UNIFORM_DIST,
+)
+parser.add_argument(
+    "-m", help="Standard deviation for distribution", type=int, default=50
+)
 
 args = parser.parse_args()
 
@@ -40,133 +59,176 @@ Y_HIGH = args.yh
 NUM_QUERIES = args.n
 NUM_SORTED = int(args.s * NUM_QUERIES)
 NUM_RANDOM = int(NUM_QUERIES - NUM_SORTED)
-INSERT_SYMBOL = 'i'
+INSERT_SYMBOL = "i"
 
-QUERY_FILE = "data/insert_s={selectivity}_{suffix}.txt".format(selectivity = args.s, suffix = f"curve={args.c}" if args.c else f"dist={args.d}")
+QUERY_FILE = "final_data/insert_s={selectivity}_{suffix}.txt".format(
+    selectivity=args.s, suffix=f"curve={args.c}" if args.c else f"dist={args.d}"
+)
+
 
 # Functions
 def print_parameters():
     print("Input Parameters")
     for action in parser._actions:
-        if action.dest != 'help':
-            option_strings = ', '.join(action.option_strings)
+        if action.dest != "help":
+            option_strings = ", ".join(action.option_strings)
             print(f"{option_strings} [{action.help}]: {getattr(args, action.dest)}")
 
-def generate_distribution_points(distribution):
-    if distribution == UNIFORM_DIST:
-        x = np.random.uniform(X_LOW, X_HIGH, NUM_SORTED)
-        y = np.random.uniform(Y_LOW, Y_HIGH, NUM_SORTED)
-
-    elif distribution == CLUSTERED_DIST:
-        x = np.concatenate([np.random.normal(X_LOW, 30, NUM_SORTED // 2),
-                            np.random.normal(X_HIGH, 30, NUM_SORTED // 2)])
-        y = np.concatenate([np.random.normal(Y_LOW, 40, NUM_SORTED // 2),
-                            np.random.normal(Y_HIGH, 50, NUM_SORTED // 2)])
-    return zipper(x, y, NUM_SORTED)
-
-def zipper(x, y, n):
-    points = Queue(maxsize=n)
-    for xval, yval in zip(x, y):
-        points.put((xval, yval))
-    return points
 
 def cubic_bezier(t, p0, p1, p2, p3):
-    return (1-t)**3 * p0 + 3*(1-t)**2*t * p1 + 3*(1-t)*t**2 * p2 + t**3 * p3
+    return (
+        (1 - t) ** 3 * p0
+        + 3 * (1 - t) ** 2 * t * p1
+        + 3 * (1 - t) * t**2 * p2
+        + t**3 * p3
+    )
+
 
 def generate_bezier_curve():
-    x_points = []
-    y_points = []
-    t_values = np.linspace(0, 1, NUM_SORTED)
-
+    curve_x = []
+    curve_y = []
+    dist_x = []
+    dist_y = []
+    t_values = np.linspace(0, 1, NUM_QUERIES)
     p0 = np.array([X_LOW, Y_LOW])
     p1 = np.array([X_LOW * 0.25, Y_HIGH * 1.5])
     p2 = np.array([X_HIGH * 0.25, Y_LOW * 1.5])
     p3 = np.array([X_HIGH, Y_HIGH])
+    index = 0
+    with open(QUERY_FILE, "w") as query_file:  # , open(DATA_FILE, 'w') as data_file:
+        for _ in range(NUM_QUERIES):
+            choice = random.random()
+            x, y = cubic_bezier(t_values[index], p0, p1, p2, p3)
+            if choice > args.s:  # generate randomly
+                xr = None
+                yr = None
+                if args.d == UNIFORM_DIST:
+                    xr = random.uniform(X_LOW, X_HIGH)
+                    yr = random.uniform(Y_LOW, Y_HIGH)
+                elif args.d == GAUSSIAN_DIST:
+                    xr = random.normalvariate(x, (X_HIGH - X_LOW) / int(args.m))
+                    yr = random.normalvariate(y, (Y_HIGH - Y_LOW) / int(args.m))
+                dist_x.append(xr)
+                dist_y.append(yr)
+                index = index + 1
 
-    for t in t_values:
-        x, y = cubic_bezier(t, p0, p1, p2, p3)
-        x_points.append(x)
-        y_points.append(y)
-    return x_points, y_points
+                tree.insert((xr, yr), data="red")
+                query_file.write("{} {} {}\n".format(INSERT_SYMBOL, xr, yr))
+            else:  # get point on the curve
+                curve_x.append(x)
+                curve_y.append(y)
+                index = index + 1
+
+                tree.insert((x, y), data="green")
+                query_file.write("{} {} {}\n".format(INSERT_SYMBOL, x, y))
+
+    return curve_x, curve_y, dist_x, dist_y
+
+
+def fermat_spiral(theta):
+    a = 5  # scaling factor
+    return a * np.sqrt(theta)
+
 
 def generate_fermat_spiral():
-    x_points = []
-    y_points = []
-    for i in range(NUM_SORTED):
-        theta = 0.1 * i
-        r = np.sqrt(theta)
-        x = r * np.cos(theta)
-        y = r * np.sin(theta)
-        x_points.append(x)
-        y_points.append(y)
-    return x, y
+    curve_x = []
+    curve_y = []
+    dist_x = []
+    dist_y = []
+
+    theta = np.linspace(0.1, 10 * np.pi, NUM_QUERIES)
+    r = np.sqrt(theta)
+    x_scaled = 150 * r * np.cos(theta)
+    y_scaled = 150 * r * np.sin(theta)
+
+    index = 0
+    with open(QUERY_FILE, "w") as query_file:
+        for _ in range(NUM_QUERIES):
+            choice = random.random()
+            x = x_scaled[index]
+            y = y_scaled[index]
+
+            if choice > args.s:  # generate randomly
+                xr = None
+                yr = None
+                if args.d == UNIFORM_DIST:
+                    xr = random.uniform(X_LOW, X_HIGH)
+                    yr = random.uniform(Y_LOW, Y_HIGH)
+                elif args.d == GAUSSIAN_DIST:
+                    xr = random.normalvariate(x, (X_HIGH - X_LOW) / int(args.m))
+                    yr = random.normalvariate(y, (Y_HIGH - Y_LOW) / int(args.m))
+                dist_x.append(xr)
+                dist_y.append(yr)
+                # tree.insert((xr, yr), data="red")
+                query_file.write("{} {} {}\n".format(INSERT_SYMBOL, xr, yr))
+            else:  # get point on the curve
+                curve_x.append(x)
+                curve_y.append(y)
+                # tree.insert((x, y), data="green")
+                query_file.write("{} {} {}\n".format(INSERT_SYMBOL, x, y))
+            index = index + 1
+
+    return curve_x, curve_y, dist_x, dist_y
+
 
 def generate_random_curve():
     # Generate theta values
-    theta = np.linspace(0, 4 * np.pi, NUM_SORTED)
-
+    theta = np.linspace(0, 4 * np.pi, NUM_QUERIES)
     # Define parameters for randomness
     # a = np.random.uniform(0.5, 2.0)  # amplitude of the sine function
     # b = np.random.uniform(0.5, 2.0)  # amplitude of the cosine function
     # c = np.random.uniform(0.5, 2.0)  # frequency of the sine function
     # d = np.random.uniform(0.5, 2.0)  # frequency of the cosine function
-
     c = 1
     d = 0.9
     # Calculate x and y coordinates
     x = X_LOW + (X_HIGH - X_LOW) * ((np.sin(c * theta) + 1) / 2)
     y = Y_LOW + (Y_HIGH - Y_LOW) * ((np.cos(d * theta) + 1) / 2)
 
+    for xl, yl in zip(x, y):
+        tree.insert((xl, yl), data="green")
 
     # Add coordinates to the queue
     return x, y
 
+
 def generate_circular_arc():
     theta = np.linspace(0, np.pi, NUM_SORTED)
-    r = np.sqrt((X_HIGH - X_LOW)**2 + (Y_HIGH - Y_LOW)**2) / 2
+    r = np.sqrt((X_HIGH - X_LOW) ** 2 + (Y_HIGH - Y_LOW) ** 2) / 2
     x = X_LOW + r * np.cos(theta)
     y = Y_LOW + r * np.sin(theta)
-    
+    for xl, yl in zip(x, y):
+        tree.insert((xl, yl), data="green")
+
     return x, y
 
-def write_points_to_query_file(x, y): 
-    with open(QUERY_FILE, 'w') as query_file: #, open(DATA_FILE, 'w') as data_file:
-        for _ in tqdm(range(NUM_QUERIES)):
-        # for _ in (range(NUM_QUERIES)):
-            index = 0
-            choice = random.random()
-            if choice > args.s: # generate randomly
-                query_file.write("{} {} {}\n".format(INSERT_SYMBOL, random.uniform(X_LOW, X_HIGH), random.uniform(Y_LOW, Y_HIGH)))
-            else: # get point on the curve
-                query_file.write("{} {} {}\n".format(INSERT_SYMBOL, x[index], y[index]))
-                index = index + 1
 
-def plot_points(curve_x, curve_y, curve_type:str):
-    # Plotting
-    # plt.plot(*zip(p0, c0, c1, p1), 'ro-')  # Plot control points and endpoints
-    plt.plot(curve_x, curve_y, label='Curve Plot')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title(f"{curve_type} plot")
-    plt.legend()
-    plt.grid(True)
-    plt.axis('equal')
-    plt.show()
-
-def get_curve_points(curve_type:str):
+def get_curve_points(curve_type: str):
     if curve_type == BEZIER:
         return generate_bezier_curve()
     elif curve_type == FMT_SPR:
         return generate_fermat_spiral()
-    elif curve_type == RND_CRV:
-        return generate_random_curve()
-    elif curve_type == CIRC_ARC:
-        return generate_circular_arc()
+    # elif curve_type == RND_CRV:
+    #     return generate_random_curve()
+    # elif curve_type == CIRC_ARC:
+    #     return generate_circular_arc()
+
 
 # Functions End
 
 # Main start
+
 print_parameters()
-x, y = get_curve_points(args.c)
-plot_points(x, y, args.c)
-write_points_to_query_file(x, y)
+curve_x, curve_y, dist_x, dist_y = get_curve_points(args.c)
+
+# Plot points
+plt.scatter(curve_x, curve_y, color="green")
+plt.scatter(dist_x, dist_y, color="red")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.legend()
+plt.grid(True)
+plt.axis("equal")
+plt.show()
+
+# quads.visualize(tree)
